3ed53eeca72633dfe6e4f78f28a78fb53f839b90
AprilNEA <github@sku.moe>
2026-07-05 06:38:52 +0800

    refactor(workbench,desktop,webview): move connection form onto react-hook-form + zod

--- apps/webview/src/routes/settings/connection.tsx
+++ apps/webview/src/routes/settings/connection.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { rhfErrorsToFormErrors } from '@webview/lib/form';
+import { rhfErrorsToFormErrors } from '@linkcode/workbench';
 import { useSettingsStore } from '@webview/settings/store';
 import { Button } from 'coss-ui/components/button';
 import { Field, FieldDescription, FieldError, FieldLabel } from 'coss-ui/components/field';
