93d720fea6ccf2e6ee6b4974547305bff819b43f
AprilNEA <github@sku.moe>
2026-07-16 12:11:41 +0800

    feat(engine): add loop leaf primitives (shell-exec, ring-buffer, loop-store)

--- packages/engine/src/automation/index.ts
+++ packages/engine/src/automation/index.ts
@@ -1,3 +1,5 @@
+export { InMemoryLoopStore, type LoopStore } from './loop-store';
+export { RingBuffer } from './ring-buffer';
 export {
   ScheduleService,
   type ScheduleServiceOptions,
@@ -5,6 +7,7 @@ export {
 } from './schedule-service';
 export { InMemoryScheduleStore, type ScheduleStore } from './schedule-store';
 export type { SessionDriver, TurnResult } from './session-driver';
+export { runShellCheck, type ShellCheckOptions, type ShellCheckResult } from './shell-exec';
 export {
   extractJson,
   promptForStructured,
